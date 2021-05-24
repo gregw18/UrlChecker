255c1e46d3b4d483067a5fc161820309f7f4ba13
Greg Walker <greg_walker@sympatico.ca>
2021-05-24 09:44:29 -0400

    Proof of concept for dynamic scraping
    Used Chrome and WebDriver to try dynamic scraping for a site that was filling
    in data fields using javascript. Works, but installing chrome in a windows
    Azure functions environment, or switching to a linux environment, isn't worth
    it to me at this time, so about to comment out code.

--- func/UrlChecker.cs
+++ func/UrlChecker.cs
@@ -9,6 +9,13 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+//using OpenQA.Selenium.WebDriver.PhantomJS;
+//using OpenQA.Selenium.WebDriver;
+
 
 // Azure function, timer triggered, that checks whether a given web page has changed
 // since the last time it was checked. If yes, sends an email to that effect.
@@ -37,8 +44,9 @@ namespace GAWUrlChecker
                 // LogEnvStrings();
 
                 string previousContentsFileName = ConfigValues.GetValue("lastChangedFileName");
-                var myChecker = new UrlCheckManager();
-                bool result = await myChecker.HaveAnyPagesChanged(previousContentsFileName);
+                //var myChecker = new UrlCheckManager();
+                //bool result = await myChecker.HaveAnyPagesChanged(previousContentsFileName);
+                TestDynamicScraping();
                 LoggerFacade.LogInformation("Finished Run.");
             }
             catch (Exception ex)
@@ -47,6 +55,44 @@ namespace GAWUrlChecker
             }
         }
 
+        // Proof of concept, using WebDriver and headless chrome to do scraping of dynamic web page.
+        // Investigated because my second desired target site, while externally identical in layout
+        // to the first site, incorporates a lot more dynamic data, including the "dateModified" field
+        // that I was targeting.
+        // Ended up not using because doesn't appear to be easy way to install chrome in a windows-based
+        // azure function environment, and it turned out that the second target site updates once a week
+        // at a defined time, so I don't really need to track it.
+        // It does seem that could install chrome in a linux environment to get this to work, but not
+        // worth the effort at this time. https://anthonychu.ca/post/azure-functions-headless-chromium-puppeteer-playwright/
+        private static void TestDynamicScraping()
+        {
+                string url = "https://health-infobase.canada.ca/covid-19/vaccination-coverage/";
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("headless");
+                using (IWebDriver driver = new ChromeDriver(options))
+                {
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    driver.Navigate().GoToUrl(url);
+                    var source = driver.PageSource;
+                    // WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    var pathElement = driver.FindElement(By.ClassName("dateModified"));
+                    LoggerFacade.LogInformation($"className={pathElement}");
+                    //var newelem = pathElement.GetAttribute("dateModified");
+                    //LoggerFacade.LogInformation($"attribute newelem={newelem}");
+                    //var newelem = pathElement.GetProperty("dateModified");
+                    //LoggerFacade.LogInformation($"property newelem={newelem}");
+                    var stuff = pathElement.Text;
+                    LoggerFacade.LogInformation($"dateModified={stuff}");
+                    //#var newElem = pathElement.FindElement(By.Name("dateModified"));
+                    //LoggerFacade.LogInformation($"source={source}");
+                    //var pathElement = driver.FindElement(By.Name("dateModified"));
+                    //LoggerFacade.LogInformation($"dm.dm={pathElement}");
+                    //var pathElement = driver.FindElement(By.Id("wb-dtmd"));
+                    //LoggerFacade.LogInformation($"wb-dtmd={pathElement}");
+                    //#pathElement = driver.FindElement(By.CssSelector("#dateModified #dateModified"));
+                    //#LoggerFacade.LogInformation($"#dm #dm={pathElement}");
+                }
+        }
 /*
         public static async Task<bool> CheckUrls(string lastChangedFileName)
         {
