aa3483258770805f866734fd07bd9bf7e80a98e4
Greg Walker <greg_walker@sympatico.ca>
2021-06-10 14:16:29 -0400

    Modified run*.bat to remove duplication.
    Had the url in the runAzure and runLocal batch files duplicated,
    once for normal and once for proxy. Since modified to use the batch.cfg,
    decided that it was a good time to also remove the duplication.

--- runAzure.sample.bat
+++ runAzure.sample.bat
@@ -1,6 +1,13 @@
 REM run the UrlChecker azure function, on azure.
 REM If receive t as an option, use a proxy, so that fiddler can snoop.
 
+REM @ECHO off
+SETLOCAL
+CALL readcfg.bat batch.cfg
+@ECHO on
+
+set liveUrl=https://%function%.azurewebsites.net/admin/functions/UrlChecker
+
 if [%1] == [] goto noproxy
 if %1==t (goto proxy) else (goto noproxy)
 
@@ -10,7 +17,7 @@ curl --header "Content-Type: application/json" ^
 	--request POST ^
 	-x 127.0.0.1:8888 ^
 	--data {input:\"test\"} ^
-	https://UrlCheckerGAW.azurewebsites.net/admin/functions/UrlChecker
+	%liveUrl%
 goto eof
 
 :noproxy
@@ -18,6 +25,6 @@ curl --header "Content-Type: application/json" ^
 	--header "x-functions-key: <your _master App key here>" ^
 	--request POST ^
 	--data {input:\"test\"} ^
-	https://UrlCheckerGAW.azurewebsites.net/admin/functions/UrlChecker
+	%liveUrl%
 
 :eof
--- runLocal.bat
+++ runLocal.bat
@@ -1,14 +1,18 @@
 REM Run the UrlChecker azure function locally.
 REM If receive t as an option, use a proxy, so that fiddler can snoop.
 
+SETLOCAL
+
+set testUrl=http://127.0.0.1:7071/admin/functions/UrlChecker
+
 if [%1] == [] goto noproxy
 if %1==t (goto proxy) else (goto noproxy)
 
 :proxy
-curl --header "Content-Type: application/json" --request POST -x 127.0.0.1:8888 --data {input:\"test\"} http://127.0.0.1:7071/admin/functions/UrlChecker
+curl --header "Content-Type: application/json" --request POST -x 127.0.0.1:8888 --data {input:\"test\"} %testUrl%
 goto eof
 
 :noproxy
-curl --header "Content-Type: application/json" --request POST --data {input:\"test\"} http://127.0.0.1:7071/admin/functions/UrlChecker
+curl --header "Content-Type: application/json" --request POST --data {input:\"test\"} %testUrl%
 
 :eof
